b982b998532b95089761b457808d7d7813c8518d
Denis Grabina <denisgrabina@gmail.com>
2019-11-21 17:34:30 +0200

    4+1 land is ready

--- start.php
+++ start.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template name: Start
  *
@@ -14,6 +15,11 @@
 
 get_header();
 
-get_template_part('template-parts/content', 'start');
+if (is_front_page() || is_page('start-bazabet')) {
+	get_template_part('template-parts/content', 'start');
+} 
+if (is_page('four-plus-one')) {
+	get_template_part('template-parts/promo/content', 'four-plus-one');
+}
 
 get_footer();
