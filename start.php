85a90bfe956803d49c5530f0aff5a5f0174177fe
Denis Grabina <denisgrabina@gmail.com>
2019-11-22 10:43:55 +0200

    add temp solution

--- start.php
+++ start.php
@@ -17,8 +17,14 @@ get_header();
 
 if (is_front_page() || is_page('start-bazabet')) {
 	get_template_part('template-parts/content', 'start');
-} 
-if (is_page('four-plus-one')) {
+}
+
+if (
+	is_page('four-plus-one') ||
+	is_page('four-plus-one-isport') ||
+	is_page('four-plus-one-football') ||
+	is_page('four-plus-one-exoclick')
+) {
 	get_template_part('template-parts/promo/content', 'four-plus-one');
 }
 
